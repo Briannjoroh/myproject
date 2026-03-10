9bdd354d9cf2129f5c3c677cb4c689463d3671f3
Brian <b10627576@gmail.com>
2026-03-10 08:12:19 +0300

    files updated

---
+++ first attempt/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14159
+
+int main()
+{
+    int age = 23;
+    printf("The AGE is : %d\n", age);
+
+    age = 30;
+    printf("The new AGE is : %f\n", age);
+
+    char letter = 'A';
+    printf("These is tyhe first letter of the alphabets: %c", letter);
+
+
+   /* float radius = 6.5;
+    float area = AREA(radius);
+
+    printf("AREA of the circle is : %f", area);
+    return 0;
+
+    float radius;
+    float volume;
+    printf("Enter the radius: ");
+    scanf("$f", &radius);
+
+    volume = (4.0/3.0) * PI * radius * radius * radius;
+
+    printf("The volume is = %.2f\n", volume);*/
+
+
+    return 0;
+
+
+}
